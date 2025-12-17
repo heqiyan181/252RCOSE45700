3875500e3cf102c3718787a4cae819a38158e696
heqiyan181 <2682659316@qq.com>
2025-12-17 21:32:16 +0900

    Game level connection

--- Assets/Scripts/Level1Win.cs
+++ Assets/Scripts/Level1Win.cs
@@ -5,6 +5,7 @@ public class Level1Win : MonoBehaviour
 {
     public void Win()
     {
+        Time.timeScale = 1f;          // ⭐ 
         PlayerPrefs.SetInt("Unlock", 2);
         PlayerPrefs.Save();
         SceneManager.LoadScene("LevelSelect");
--- Assets/Scripts/Spawner/Spawner.cs
+++ Assets/Scripts/Spawner/Spawner.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -13,41 +12,45 @@ public enum SpawnModes
 public class Spawner : MonoBehaviour
 {
     public static Action OnWaveCompleted;
-    
+
     [Header("Settings")]
     [SerializeField] private SpawnModes spawnMode = SpawnModes.Fixed;
     [SerializeField] private int enemyCount = 10;
     [SerializeField] private float delayBtwWaves = 1f;
 
+    // ✅ 修改这里：最后一波 = 10（因为你只有10波）
+    [Header("Win Settings")]
+    [SerializeField] private int finalWave = 10;
+
     [Header("Fixed Delay")]
     [SerializeField] private float delayBtwSpawns;
-    
+
     [Header("Random Delay")]
     [SerializeField] private float minRandomDelay;
     [SerializeField] private float maxRandomDelay;
 
-    [Header("Poolers")] 
+    [Header("Poolers")]
     [SerializeField] private ObjectPooler enemyWave10Pooler;
+    // 如果你删除了其他波数，可以删除或禁用这些引用
     [SerializeField] private ObjectPooler enemyWave11To20Pooler;
-    [SerializeField] private ObjectPooler enemyWave21To30Pooler;
-    [SerializeField] private ObjectPooler enemyWave31To40Pooler;
-    [SerializeField] private ObjectPooler enemyWave41To50Pooler;
-    
+
     private float _spawnTimer;
     private int _enemiesSpawned;
     private int _enemiesRamaining;
-    
+
     private Waypoint _waypoint;
+    private bool _levelFinished = false;
 
     private void Start()
     {
         _waypoint = GetComponent<Waypoint>();
-
         _enemiesRamaining = enemyCount;
     }
 
     private void Update()
     {
+        if (_levelFinished) return;
+
         _spawnTimer -= Time.deltaTime;
         if (_spawnTimer < 0)
         {
@@ -62,7 +65,14 @@ public class Spawner : MonoBehaviour
 
     private void SpawnEnemy()
     {
-        GameObject newInstance = GetPooler().GetInstanceFromPool();
+        ObjectPooler pooler = GetPooler();
+        if (pooler == null) 
+        {
+            // 如果获取不到 pooler，使用默认的
+            pooler = enemyWave10Pooler;
+        }
+
+        GameObject newInstance = pooler.GetInstanceFromPool();
         Enemy enemy = newInstance.GetComponent<Enemy>();
         enemy.Waypoint = _waypoint;
         enemy.ResetEnemy();
@@ -73,74 +83,66 @@ public class Spawner : MonoBehaviour
 
     private float GetSpawnDelay()
     {
-        float delay = 0f;
-        if (spawnMode == SpawnModes.Fixed)
-        {
-            delay = delayBtwSpawns;
-        }
-        else
-        {
-            delay = GetRandomDelay();
-        }
-
-        return delay;
+        if (spawnMode == SpawnModes.Fixed) return delayBtwSpawns;
+        return GetRandomDelay();
     }
-    
+
     private float GetRandomDelay()
     {
-        float randomTimer = Random.Range(minRandomDelay, maxRandomDelay);
-        return randomTimer;
+        return Random.Range(minRandomDelay, maxRandomDelay);
     }
 
     private ObjectPooler GetPooler()
     {
         int currentWave = LevelManager.Instance.CurrentWave;
-        if (currentWave <= 10) // 1- 10
-        {
-            return enemyWave10Pooler;
-        }
 
-        if (currentWave > 10 && currentWave <= 20) // 11- 20
-        {
-            return enemyWave11To20Pooler;
-        }
+        // 简化：所有波都用同一个 pooler
+        return enemyWave10Pooler;
         
-        if (currentWave > 20 && currentWave <= 30) // 21- 30
-        {
-            return enemyWave21To30Pooler;
-        }
-        
-        if (currentWave > 30 && currentWave <= 40) // 31- 40
-        {
-            return enemyWave31To40Pooler;
-        }
-        
-        if (currentWave > 40 && currentWave <= 50) // 41- 50
-        {
-            return enemyWave41To50Pooler;
-        }
-
-        return null;
+        /* 或者保留两波：
+        if (currentWave <= 10) return enemyWave10Pooler;
+        return enemyWave11To20Pooler;
+        */
     }
-    
+
     private IEnumerator NextWave()
     {
         yield return new WaitForSeconds(delayBtwWaves);
+
+        if (_levelFinished) yield break;
+
         _enemiesRamaining = enemyCount;
         _spawnTimer = 0f;
         _enemiesSpawned = 0;
     }
-    
+
     private void RecordEnemy(Enemy enemy)
     {
+        if (_levelFinished) return;
+
         _enemiesRamaining--;
         if (_enemiesRamaining <= 0)
         {
+            int currentWave = LevelManager.Instance.CurrentWave;
+
+            // ✅ 这里：第10波结束就胜利
+            if (currentWave >= finalWave) // finalWave = 10
+            {
+                _levelFinished = true;
+
+                // 显示胜利画面
+                ShowVictory sv = FindObjectOfType<ShowVictory>();
+                if (sv != null) sv.Show();
+
+                return;
+            }
+
+            // 正常进入下一波
             OnWaveCompleted?.Invoke();
             StartCoroutine(NextWave());
         }
     }
-    
+
     private void OnEnable()
     {
         Enemy.OnEndReached += RecordEnemy;
